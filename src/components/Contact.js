9de60380c550c88e85a8f2b52aa14fe9d47d00ff
unknown <aljayfauni@gmail.com>
2023-02-02 12:59:13 +0800

    remove phone

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -57,12 +57,9 @@ export default function Contact() {
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                email@email.com
+                Use Contact Form
               </a>
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
-                PHONE
-              </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+             
             </div>
           </div>
         </div>
